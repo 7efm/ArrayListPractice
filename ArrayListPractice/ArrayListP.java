b6bcec11c150ce0ccc2009f9ac9a5eb1bc66318a
7efm <38299622+7efm@users.noreply.github.com>
2019-01-23 13:02:17 -0800

    #8

--- ArrayListPractice/ArrayListP.java
+++ ArrayListPractice/ArrayListP.java
@@ -10,14 +10,23 @@ import java.util.List;
 public class ArrayListP
 {
     ArrayList<String> colors;
+    ArrayList<Integer> numbers;
     // 1
     public ArrayListP(){
         colors = new ArrayList<String>();
+        numbers = new ArrayList<Integer>();
         colors.add("Red");
         colors.add("Blue");
         colors.add("Green");
         colors.add("Mahogany");
         for(String str : colors){
+            System.out.println(str);
+        }
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        for(int str : numbers){
             System.out.println(str);
         }
     }
@@ -44,8 +53,26 @@ public class ArrayListP
         colors.remove(2);
     }
     // 7
-    public int search(){
+    public int search(String searchElement){
         int index = 0;
-        return index;
+        for(String str : colors){
+            if(str.equals(searchElement)){
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+    // 8
+    public void sortByLengthD(){
+        int bigger;
+        int smaller;
+        for(int i = 0; i <= numbers.size(); i++){
+            if(numbers.get(i) < numbers.get(i+1)){
+                smaller = numbers.get(i); bigger = numbers.get(i+1);
+                numbers.set(i, bigger); numbers.set(i+1, smaller);
+            }
+        }
+
     }
 }
